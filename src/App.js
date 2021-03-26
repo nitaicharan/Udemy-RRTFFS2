37eddab1683cc3573cba321f86d8469e68842da2
Nitai Charan <nitaicharan@gmail.com>
2021-03-26 00:00:00 -0300

    29. Conectando nuestros componentes con redux

--- src/App.js
+++ src/App.js
@@ -1,25 +1,39 @@
-import logo from './logo.svg';
+import React, { Component } from "react";
+import { connect } from "react-redux";
 import './App.css';
+import logo from './logo.svg';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+class App extends Component {
+  render() {
+    console.log(this.props);
+
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>
+            Edit <code>src/App.js</code> and save to reload.
+          </p>
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn React
+          </a>
+        </header>
+      </div>
+    );
+  }
 }
 
-export default App;
+const mapSateToProps = state => {
+  console.log({ state });
+  return { valor: state };
+};
+
+const mapDispatchToProp = dispatch => ({});
+
+export default connect(mapSateToProps, mapDispatchToProp)(App);
