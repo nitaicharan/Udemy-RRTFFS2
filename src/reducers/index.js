51df1deab6d684442c206458339c11a0bbc7bde2
Nitai Charan <nitaicharan@gmail.com>
2021-03-27 22:53:38 -0300

    27. Action creators

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,14 +1,21 @@
 const INCREASE = 'COUNTER/INCREASE';
 const DECREASE = 'COUNTER/DECREASE';
+const SET = 'COUTER/SET';
+
+const increase = () => ({ type: INCREASE });
+const decrease = () => ({ type: DECREASE });
+const set = payload => ({ type: SET, payload });
 
 const initialState = 0;
-export default function(state = initialState, action) {
+export default function (state = initialState, action) {
     switch (action.type) {
         case INCREASE:
             return state + 1;
         case DECREASE:
             return state - 1;
+        case SET:
+            return action.payload;
         default:
-            return state ;
+            return state;
     }
 }
